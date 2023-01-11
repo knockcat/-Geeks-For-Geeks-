c9c196e3aaf04f276b9c025f9fbe938141a862c5
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-01-11 21:23:43 +0530

    Create Make array elements unique.cpp

---
+++ Make array elements unique.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long int minIncrements(vector<int> arr, int N) {
+        // Code here
+         long long int ans=0;
+
+        unordered_map<int,int>mp;
+
+        for(int i=0;i<N;i++){
+            while(mp.count(arr[i])){
+                arr[i]++;
+                ans++;
+            }
+            mp[arr[i]]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minIncrements(arr, N) << endl;
+    }
+}
+// } Driver Code Ends
