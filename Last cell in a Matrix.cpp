c94d624979a8318c33d7492eed502b2932e5b451
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-02-04 00:01:36 +0530

    Create Last cell in a Matrix.cpp

---
+++ Last cell in a Matrix.cpp
@@ -0,0 +1,16 @@
+ // right, down, left, up ->array
+        vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int n = matrix.size(), m = matrix[0].size();
+        int i=0, j=0, k=0;
+        pair<int, int> ans;
+        while(i>=0 && j>=0 && i< n && j<m){
+            ans = {i, j};
+            if(matrix[i][j]==1){
+                matrix[i][j]=0;
+                // using modulus so our index will never go out of bound
+                k = (k+1)%4; 
+            }
+            i = i+dir[k].first;
+            j = j+dir[k].second;
+        }
+        return ans;
