e679db4ef34767cf0a9a2239cfc25005ce29503c
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-02-09 09:49:12 +0530

    Create Burst Balloons.cpp

---
+++ Burst Balloons.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+public:
+
+    int helper(int i, int j , vector<int>& arr, vector<vector<int>>& dp)
+    {
+        if(i > j)
+            return 0;
+            
+        if(dp[i][j] != -1)
+            return dp[i][j];
+            
+        int ans = INT_MIN;
+        for(int idx = i; idx <= j; ++idx)
+        {
+            int cost = arr[i-1] * arr[idx] * arr[j+1] + helper(i, idx-1, arr, dp) + helper(idx+1, j, arr, dp);
+            ans = max(ans,cost);
+        }
+        
+        return dp[i][j] = ans;
+    }
+
+    int maxCoins(int N, vector<int> &arr) {
+        // code here
+        
+        int n = N;
+        arr.insert(arr.begin(),1);
+        arr.push_back(1);
+        
+        // vector<vector<int>> dp(n+2, vector<int>(n+2,-1));
+        
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+        
+        for(int i = n; i >= 1; --i)
+        {
+            for(int j = 1; j <= n; ++j)
+            {
+                if(i > j)
+                    continue;
+                int ans = INT_MIN;
+                for(int idx = i ; idx <= j; ++idx)
+                {
+                    int cost = arr[i-1] * arr[idx] * arr[j+1] + dp[i][idx-1] + dp[idx+1][j];
+                    ans = max(ans,cost);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        
+        // return helper(1,N,arr, dp);
+        return dp[1][n];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        vector<int> arr(N);
+        for(int i=0; i<N; i++)
+            cin>>arr[i];
+
+        Solution obj;
+        cout << obj.maxCoins(N, arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
